6a73ba59f8a8ae2e0e7e6ead58ebbeb551038903
vladimir-stal <vladimirstal@gmail.com>
2023-08-24 16:16:28 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import './globals.css';
+
+export default function Home() {
+  return (
+    <main className="flex flex-row justify-center gap-2 p-2 border border-black border-dashed w-[500px] m-auto">
+      <div className="border border-solid border-black p-1">Welcome</div>
+      <div className="p-1">To</div>
+      <div className="p-1">NEXTJS</div>
+      <div className="p-1">From 0</div>
+    </main>
+  );
+}
